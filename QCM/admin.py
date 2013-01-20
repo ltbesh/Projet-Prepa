764ae8db275c7e5fc52bb4f3acec338e299ce760
ltbesh <lucastineberes@gmail.com>
2013-01-20 17:58:52 +0100

    First version of the admin site with question and answer creation

--- QCM/admin.py
+++ QCM/admin.py
@@ -1,5 +1,28 @@
-from QCM.models import Question
+from QCM.models import Question, Answer, Chapter, Subject, Level
+
 from django.contrib import admin
 
 
-admin.site.register(Question)
+class AnswerInline(admin.StackedInline):
+	model = Answer
+	extra = 4
+		
+class QuestionInline(admin.StackedInline):
+	model = Question
+	extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):	
+	fieldsets = [
+        (None,               {'fields': ['question', 'chapter', 'subject', 'level']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+	inlines = [AnswerInline]
+
+
+admin.site.register(Question,QuestionAdmin)
+admin.site.register(Answer)
+admin.site.register(Chapter)
+admin.site.register(Subject)
+admin.site.register(Level)
--- QCM/models.py
+++ QCM/models.py
@@ -6,29 +6,49 @@ from django.contrib.auth.models import User
 
 from django.db import models
 
+class Subject(models.Model):
+	name = models.CharField(max_length = 200)
+
+	def __unicode__ (self):
+		return self.name
+
+
+class Level(models.Model):
+	name = models.CharField(max_length = 200)
+
+	def __unicode__ (self):
+		return self.name
+
 
 class Chapter(models.Model):
-	subject = models.CharField(max_length = 200) #Physique, Chime, Histoire ...
-	level = models.CharField(max_length = 200) # Student class level 
 	name = models.CharField(max_length = 200) # name of the chapter : calculus, probability ...
 	
+	def __unicode__ (self):
+		return self.name
 
 class Question(models.Model):
-    pub_date = models.DateTimeField('date published')
-    #creator = models.ForeignKey(User)
-    #moderators = models.ForeignKey(User) # We need to be able to assign several moderators
-    #validator = models.ForeignKey(User) #admin qui valide le passage a la bdd live
-    
-    question = models.CharField(max_length = 2000)
-    
-    # tags = models.ForeignKey(Tags) # Used for classifying question (course question, order of magnitude ...)
+	level = models.ForeignKey(Level)
+	subject = models.ForeignKey(Subject)
+	chapter = models.ForeignKey(Chapter)
+	pub_date = models.DateTimeField('date published')
+	#creator = models.ForeignKey(User)
+	#moderators = models.ForeignKey(User) # We need to be able to assign several moderators
+	#validator = models.ForeignKey(User) #admin qui valide le passage a la bdd live
+	
+	question = models.CharField(max_length = 2000)
+	
+	# tags = models.ForeignKey(Tags) # Used for classifying question (course question, order of magnitude ...)
 
-    #chapter = models.ForeignKey(Chapter)
+	def __unicode__ (self):
+		return self.question
 
 class Answer(models.Model):
 	question = models.ForeignKey(Question)
 	answer = models.CharField(max_length = 200)
 	validity = models.BooleanField()
+
+	def __unicode__ (self):
+		return self.answer
 
 class Guess(models.Model):
 	user = models.ForeignKey(User)
