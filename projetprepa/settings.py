885a23172ee3e82322b1b49e0109c5c5309ee411
ltbesh <lucastineberes@gmail.com>
2013-01-25 17:57:11 +0100

    Cleaned Url files

--- projetprepa/settings.py
+++ projetprepa/settings.py
@@ -91,7 +91,7 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'projetprepa.urls'
+ROOT_URLCONF = 'urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'projetprepa.wsgi.application'
