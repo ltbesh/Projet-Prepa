38840cce31acb2272bf3be402c22ea010e25b97b
MathieuK <mathieukudla@gmail.com>
2013-03-05 22:09:44 +0100

    Home ok SAUF pour password/change

--- projetprepa/settings.py
+++ projetprepa/settings.py
@@ -101,6 +101,7 @@ WSGI_APPLICATION = 'projetprepa.wsgi.application'
 
 TEMPLATE_DIRS = (
     'templates',
+    'QCM/templates/QCM',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
